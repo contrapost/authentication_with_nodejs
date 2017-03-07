0c4d2cb8a59fc68ca94081ca5f7bec3d15635f1c
Alexander <shiale14@student.westerdals.no>
2017-03-07 15:15:25 +0100

    add dashboard view, body-parser, routes for logout and dashboard, post for registration form

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.set('view engine', 'jade');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
     res.render('index.jade');
@@ -11,8 +15,21 @@ app.get('/register', function(req, res) {
     res.render('register.jade')
 });
 
+app.post('/register', function(req, res) {
+    res.json(req.body);
+});
+
 app.get('/login', function(req, res) {
     res.render('login.jade')
 });
+
+app.get('/dashboard', function(req, res) {
+    res.render('dashboard.jade')
+});
+
+app.get('/logout', function(req, res) {
+    res.redirect('/')
+});
+
 
 app.listen(3000);

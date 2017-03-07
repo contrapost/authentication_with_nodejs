39e97fca26a9e21c0ca92158c3866fd42940a6fc
Alexander <shiale14@student.westerdals.no>
2017-03-07 14:44:30 +0100

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('index.jade');
+});
+
+app.get('/register', function(req, res) {
+    res.render('register.jade')
+});
+
+app.get('/login', function(req, res) {
+    res.render('login.jade')
+});
+
+app.listen(3000);
